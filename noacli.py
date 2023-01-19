0b8ac0219a16df9a798a5b6e69271da39f0f46e0
Steven Dick <kg4ydw@gmail.com>
2023-01-18 22:45:37 -0500

    v1.7

--- noacli.py
+++ noacli.py
@@ -35,7 +35,7 @@ from lib.envdatamodel import envSettings
 from lib.buttondock import ButtonDock, EditButtonDocks
 from lib.favorites import Favorites
 
-__version__ = '1.6'
+__version__ = '1.7'
 
 # Some settings have been moved to relevant modules
 class settingsDict():
