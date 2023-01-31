7a0a4a4c18a136dd8fc29d8d070480c3f166aa1b
Steven Dick <kg4ydw@gmail.com>
2023-01-30 23:39:36 -0500

    version 1.9.1 small features and bug fixes
    delete some ideas that turned out to not be necessary, reprioritize others

--- noacli.py
+++ noacli.py
@@ -35,7 +35,7 @@ from lib.envdatamodel import envSettings
 from lib.buttondock import ButtonDock, EditButtonDocks
 from lib.favorites import Favorites
 
-__version__ = '1.9'
+__version__ = '1.9.1'
 
 # Some settings have been moved to relevant modules
 class settingsDict():
